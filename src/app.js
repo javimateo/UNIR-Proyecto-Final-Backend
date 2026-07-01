13731ce882d1f2104f273e4c2546edce532dcb18
javimateo <javimateo2003@gmail.com>
2026-07-01 13:34:55 +0200

    feat: sistema de reportes y moderación de artículos

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use('/api/favorites',  require('./routes/favorites.routes'));
 app.use('/api/messages',   require('./routes/messages.routes'));
 // app.use('/api/brands',        require('./routes/brands.routes'));
 // app.use('/api/conversations', require('./routes/conversations.routes'));
-// app.use('/api/reports',       require('./routes/reports.routes'));
+app.use('/api/reports',       require('./routes/reports.routes'));
 // app.use('/api/valuations',    require('./routes/valuations.routes'));
 
 app.use(errorMiddleware);
